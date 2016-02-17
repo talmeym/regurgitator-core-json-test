58e8278648e1cae4f710bde32e68824433b7b004
talmeym <mtalmey@gmail.com>
2016-02-17 11:40:19 +0000

    added last-index flag to size processor

---
+++ src/test/java/com/emarte/regurgitator/test/SizeProcessorJsonLoaderTest.java
@@ -0,0 +1,27 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.*;
+import org.junit.Test;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+
+public class SizeProcessorJsonLoaderTest extends JsonLoaderTest {
+	public SizeProcessorJsonLoaderTest() {
+		super(new SizeProcessorJsonLoader());
+	}
+
+	@Test
+	public void testJson() throws Exception {
+		assertExpectation("classpath:/SizeProcessor.json", "com.emarte.regurgitator.core.SizeProcessor:[false]");
+	}
+
+	@Test
+	public void testLastIndex() throws Exception {
+		assertExpectation("classpath:/SizeProcessor_asIndex.json", "com.emarte.regurgitator.core.SizeProcessor:[true]");
+	}
+
+	@Test
+	public void testFullLoad() throws Exception {
+		loadFile("classpath:/SizeProcessor_fullLoad.json");
+	}
+}

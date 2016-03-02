b943ae2ca217c8386474f3ea264ee620a99920e9
talmeym <mtalmey@gmail.com>
2016-03-02 19:53:11 +0000

    simplified condition behaviour loading

--- src/test/java/com/emarte/regurgitator/test/DecisionJsonLoaderTest.java
+++ src/test/java/com/emarte/regurgitator/test/DecisionJsonLoaderTest.java
@@ -10,7 +10,7 @@ public class DecisionJsonLoaderTest extends JsonLoaderTest {
 
 	@Test
 	public void testMidJson() throws Exception {
-		assertExpectation("classpath:/Decision_min_min.json", "com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'value',true,com.emarte.regurgitator.core.ContainsBehaviour:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]");
+		assertExpectation("classpath:/Decision_min.json", "com.emarte.regurgitator.core.Decision:['decision-1',[com.emarte.regurgitator.test.stuff.TestStep:['test-step-1']],[com.emarte.regurgitator.core.Rule:['rule-1',[com.emarte.regurgitator.core.Condition:['condition-1',com.emarte.regurgitator.core.ContextLocation:['context:location'],'value',true,com.emarte.regurgitator.core.ContainsBehaviour:[]]],'test-step-1']],com.emarte.regurgitator.core.FirstMatchBehaviour:[],null]");
 	}
 
 	@Test

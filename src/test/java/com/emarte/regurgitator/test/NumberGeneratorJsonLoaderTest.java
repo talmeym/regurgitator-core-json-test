729006229773e02fe18b25a2c6914e13c9450940
talmeym <mtalmey@gmail.com>
2016-03-04 09:54:49 +0000

    fixed tests and loaders for number-generator and uuid-generator

---
+++ src/test/java/com/emarte/regurgitator/test/NumberGeneratorJsonLoaderTest.java
@@ -0,0 +1,27 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.NumberGeneratorJsonLoader;
+import org.junit.Test;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+
+public class NumberGeneratorJsonLoaderTest extends JsonLoaderTest {
+	public NumberGeneratorJsonLoaderTest() {
+		super(new NumberGeneratorJsonLoader());
+	}
+
+	@Test
+	public void testThis() throws Exception {
+		assertExpectation("classpath:/NumberGenerator.json", "com.emarte.regurgitator.core.NumberGenerator:[null]");
+	}
+
+	@Test
+	public void testMax() throws Exception {
+		assertExpectation("classpath:/NumberGenerator_max.json", "com.emarte.regurgitator.core.NumberGenerator:[1234]");
+	}
+
+	@Test
+	public void testFullLoad() throws Exception {
+		loadFile("classpath:/NumberGenerator_fullLoad.json");
+	}
+}
---
+++ src/test/java/com/emarte/regurgitator/test/UuidGeneratorJsonLoaderTest.java
@@ -0,0 +1,22 @@
+package com.emarte.regurgitator.test;
+
+import com.emarte.regurgitator.core.*;
+import org.junit.Test;
+
+import static com.emarte.regurgitator.core.ConfigurationFile.loadFile;
+
+public class UuidGeneratorJsonLoaderTest extends JsonLoaderTest {
+	public UuidGeneratorJsonLoaderTest() {
+		super(new UuidGeneratorJsonLoader());
+	}
+
+	@Test
+	public void testThis() throws Exception {
+		assertExpectation("classpath:/UuidGenerator.json", "com.emarte.regurgitator.core.UuidGenerator:[]");
+	}
+
+	@Test
+	public void testFullLoad() throws Exception {
+		loadFile("classpath:/UuidGenerator_fullLoad.json");
+	}
+}

3430a884e30e6d271aca0483789708e43bfd3216
talmeym <mtalmey@gmail.com>
2017-10-06 14:58:39 +0100

    Applied MIT License source comments

--- src/main/java/com/emarte/regurgitator/test/TestJsonPackageMap.java
+++ src/main/java/com/emarte/regurgitator/test/TestJsonPackageMap.java
@@ -5,7 +5,7 @@ import com.emarte.regurgitator.core.AbstractJsonPackageMap;
 import java.util.*;
 
 public class TestJsonPackageMap extends AbstractJsonPackageMap {
-    private static List<String> kinds = Arrays.asList("test-value-generator", "test-value-builder", "test-value-validator", "test-value-processor", "test-step", "test-condition-behaviour", "test-rules-behaviour");
+    private static final List<String> kinds = Arrays.asList("test-value-generator", "test-value-builder", "test-value-validator", "test-value-processor", "test-step", "test-condition-behaviour", "test-rules-behaviour");
 
     public TestJsonPackageMap() {
         addPackageMapping(kinds, "com.emarte.regurgitator.test.stuff");
